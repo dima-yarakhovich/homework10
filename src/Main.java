bd0abdb31ebd56b8b897e76f0d45e8b91e3d269b
dima-yarakhovich <dima020490@tut.by>
2022-09-23 22:25:24 +0300

    Сдал ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1_2();
+        task3();
+        task5();
+        task6();
+        task7();
+
+    }
+
+    private static void task5() {
+        String fullName = " Ivanov Ivan Ivanovich";
+        fullName = fullName.trim();
+        String middleName = String.valueOf(fullName.lastIndexOf(" "));
+        String firstName = String.valueOf(fullName.indexOf(" "));
+        String lastName = fullName.substring(0, Integer.parseInt(firstName));
+        firstName = fullName.substring(Integer.parseInt(String.valueOf(firstName)), Integer.parseInt(middleName));
+        middleName = fullName.substring(Integer.parseInt(middleName));
+        System.out.println("lastName = " + lastName.trim());
+        System.out.println("firstName = " + firstName.trim());
+        System.out.println("middleName = " + middleName.trim());
+
+
+    }
+
+    private static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        String fullNameE = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameE);
+
+    }
+
+    private static void task1_2() {
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("fullName = " + fullName);
+        String fullNameUp = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUp);
     }
 }
